fdf612d46a14aaf6b735de7e341e75f5689546ed
Hassaan Alvi <hassaanalvi47@gmail.com>
2025-01-27 16:41:43 +0500

    ui changes

--- calculator_project/calculator.py
+++ calculator_project/calculator.py
@@ -19,10 +19,12 @@ operations  = {
      "*":multiply,
      "/":divide,}
 continue_calculation = True
-no1 = int(input("Please input number 1:\n"))
+no1 = float(input("Please input number 1:\n"))
 while continue_calculation is True:
-    no2 = int(input("Please input number 2:\n"))
-    operator = input("Please select the operation\n")
+    no2 = float(input("Please input number 2:\n"))
+    for symbol in operations:
+        print(symbol)
+    operator = input("Please select the operation \n")
     result =operations[operator](no1,no2)
     print(f"Your calculated value is {result}")
     cont = input("would you like to make another calculation?\n")
