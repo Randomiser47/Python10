e437e3039f665301667720cbace206eaaa42b72a
Hassaan Alvi <hassaanalvi47@gmail.com>
2025-01-27 10:49:43 +0500

    New files

--- functions_with_outputs.py
+++ functions_with_outputs.py
@@ -1,2 +1,7 @@
-print("hellow world")
-print ("wowowowow")
+def format_name(f_name,l_name):
+    formatted_f_name=f_name.title()
+    formatted_l_name=l_name.title()
+    return f"{formatted_f_name} {formatted_l_name}"
+
+formatted_string = format_name("angleA", "YU")
+print(formatted_string)
